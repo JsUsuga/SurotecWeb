29fe3e72fbced115de6d633c6c7a901dc9979609
JsUsuga <juan.usuga@globant.com>
2025-09-10 14:03:27 -0500

    feat(domain): add initial entities for Offers module - Created Offer (abstract), JobOffer and AcademicOffer classes - Added JobApplication entity - Includes basic attributes, constructors and getters/setters

--- src/main/java/model/domain/offer/AcademicOffer.java
+++ src/main/java/model/domain/offer/AcademicOffer.java
@@ -1,4 +1,40 @@
 package model.domain.offer;
 
-public class AcademicOffer {
+public class AcademicOffer extends Offer {
+
+    private String institution;
+    private String program;
+
+    public AcademicOffer() {
+        super();
+    }
+
+    public AcademicOffer(Long id, String title, String description, String institution, String program) {
+        super(id, title, description);
+        this.institution = institution;
+        this.program = program;
+    }
+
+    public String getInstitution() { return institution; }
+    public void setInstitution(String institution) { this.institution = institution; }
+
+    public String getProgram() { return program; }
+    public void setProgram(String program) { this.program = program; }
+
+    @Override
+    public String getType() {
+        return "ACADEMIC";
+    }
+
+    @Override
+    public String toString() {
+        return "AcademicOffer{" +
+                "id=" + id +
+                ", title='" + title + '\'' +
+                ", description='" + description + '\'' +
+                ", institution='" + institution + '\'' +
+                ", program='" + program + '\'' +
+                ", createdAt=" + createdAt +
+                '}';
+    }
 }
