2855c4768e561b70744962a67aa5673c66baa70f
GabrielaMontilla <gabriela.montilla@globant.com>
2025-09-10 23:05:06 -0500

    Coneccion a Base de datos MySQL con clases en config de DatabaseConnection y DatabaseTest

--- src/main/java/repository/config/DatabaseConnection.java
+++ src/main/java/repository/config/DatabaseConnection.java
@@ -1,5 +1,23 @@
 package repository.config;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class DatabaseConnection {
 
+    public Connection connect() {
+        Connection connection = null;
+
+        try {
+            connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/surotec_web", "root", "123456");
+            if (connection != null) {
+                System.out.println("Conexión exitosa a la base de datos Surotec Web");
+            }
+        } catch (SQLException e) {
+            System.out.println("Error de conexión: " + e.getMessage());
+        }
+
+        return connection;
+    }
 }
